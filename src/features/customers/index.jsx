a67540174cdd57e6d42bf2b9a9eb9102e963f253
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-04-26 17:26:01 +0330

    Add an initial wrapper content box

--- src/features/customers/index.jsx
+++ src/features/customers/index.jsx
@@ -1,6 +1,50 @@
+import { Box, Fab, TextField, Typography } from '@mui/material';
+import styled from 'styled-components';
+import { Row } from '../../components/Row';
+import { ArrowBack, Refresh } from '@mui/icons-material';
+
+const StyledBox = styled(Box)`
+  --box-padding: max(2rem, 2vw);
+  --border-radius: 10px;
+
+  border: 1px solid #e3e3e3;
+  padding: var(--box-padding);
+  border-radius: var(--border-radius);
+`;
 
 export default function Customers() {
   return (
-    <div>Customers</div>
-  )
+    <StyledBox>
+      <Row>
+        <Row>
+          <ArrowBack />
+          <Typography variant="body2"> Client List </Typography>
+        </Row>
+
+        <Row sx={{ gap: '10px' }}>
+          <TextField
+            id="standard-password-input"
+            label="Search"
+            type="search"
+            variant="outlined"
+            size='small'
+          />
+          <Fab
+            variant="index.jsx"
+            size="small"
+            color="default">
+            <Refresh />
+            {/* Refresh */}
+          </Fab>
+
+          <Fab
+            variant="extended"
+            size="small"
+            color="primary">
+            Add New Client
+          </Fab>
+        </Row>
+      </Row>
+    </StyledBox>
+  );
 }

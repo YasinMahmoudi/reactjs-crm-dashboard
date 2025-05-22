d2fda685a8e465bdc2201ada34fe97c49a4dee8b
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-05-22 13:38:43 +0330

    Add Prefetching data for cutomers hook

--- src/features/customers/useGetCustomers.js
+++ src/features/customers/useGetCustomers.js
@@ -1,18 +1,33 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getCustomersService } from '../../services/customers/customer';
 import { useSearchParams } from 'react-router';
 
 function useGetCustomers() {
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
 
-  const query = searchParams.get('query');
+  const query = searchParams.get('query') || null;
 
-  const page = searchParams.get('page') ?? 1;
+  const page = parseInt(searchParams.get('page') ?? 1);
 
   const { data, isLoading: isLoadingCustomers } = useQuery({
     queryKey: ['customers', page, query],
     queryFn: () => getCustomersService({ page, query }),
   });
+
+  if (page < data?.pagination.pages) {
+    queryClient.prefetchQuery({
+      queryKey: ['customers', page + 1, query],
+      queryFn: () => getCustomersService({ page: page + 1, query }),
+    });
+  }
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['customers', page - 1, query],
+      queryFn: () => getCustomersService({ page: page - 1, query }),
+    });
 
   return {
     customers: data?.result,
--- src/services/customers/customer.js
+++ src/services/customers/customer.js
@@ -1,6 +1,6 @@
 import { API_URL } from '../../utils/constants';
 
-export const LIMIT_ITEMS = 5;
+export const LIMIT_ITEMS = 1;
 
 export async function getCustomersService({ page = 1, query = '' }) {
   const searchableFields = ['name'];

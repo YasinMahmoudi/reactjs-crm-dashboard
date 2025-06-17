7cc3ecceab740b0f2e22338cd3788d092b24e450
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-06-18 01:08:46 +0330

    Add basic inputs of invoice create page

--- src/features/invoices/InvoiceCreate.jsx
+++ src/features/invoices/InvoiceCreate.jsx
@@ -1,7 +1,16 @@
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Divider from '@mui/material/Divider';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Select from '@mui/material/Select';
 import { Controller, useForm } from 'react-hook-form';
 import FormInput from '../../components/FormInput';
 import InvoiceCreateToolbar from './InvoiceCreateToolbar';
-import { Button, Grid } from '@mui/material';
 
 export default function InvoiceCreate() {
   const { control, handleSubmit } = useForm();
@@ -18,9 +27,9 @@ export default function InvoiceCreate() {
         <Grid
           container
           spacing={{ xs: 2, md: 2 }}
-          columns={{ xs: 1, sm: 4 }}
+          columns={{ xs: 1, sm: 4, md: 6 }}
           mt={5}>
-          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+          <Grid size={{ xs: 2, sm: 2, md: 3 }}>
             <Controller
               name="client"
               defaultValue=""
@@ -29,16 +38,23 @@ export default function InvoiceCreate() {
                 required: 'Please add a client .',
               }}
               render={(field) => (
-                <FormInput
-                  label="Client"
-                  control={control}
+                <Autocomplete
+                  disablePortal
+                  options={['Test 1', 'Test 2', 'Test 3', 'Test 4']}
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Movie"
+                    />
+                  )}
                   {...field}
                 />
               )}
             />
           </Grid>
 
-          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+          <Grid size={{ xs: 2, sm: 2, md: 1 }}>
             <Controller
               name="number"
               defaultValue=""
@@ -54,6 +70,112 @@ export default function InvoiceCreate() {
                 />
               )}
             />
+          </Grid>
+
+          <Grid size={{ xs: 2, sm: 2, md: 1 }}>
+            <Controller
+              name="year"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: 'Please select a year .',
+              }}
+              render={(field) => (
+                <FormInput
+                  label="Year"
+                  control={control}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid size={{ xs: 2, sm: 2, md: 1 }}>
+            <Controller
+              name="status"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: 'Please select a status',
+              }}
+              render={(field) => (
+                <FormControl
+                  fullWidth
+                  {...field}>
+                  <InputLabel id="demo-simple-select-label">Status</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={''}
+                    label="Status"
+                    onChange={{}}>
+                    <MenuItem value="draft"> Draft </MenuItem>
+                    <MenuItem value="pending"> Pending </MenuItem>
+                    <MenuItem value="sent"> Sent </MenuItem>
+                  </Select>
+                </FormControl>
+              )}
+            />
+          </Grid>
+
+          <Grid size={{ xs: 2, sm: 2, md: 3 }}>
+            <Controller
+              name="date"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: 'Please select a date .',
+              }}
+              render={(field) => (
+                <FormInput
+                  label="Date"
+                  control={control}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid size={{ xs: 2, sm: 2, md: 1 }}>
+            <Controller
+              name="expireDate"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: 'Please select an expire date .',
+              }}
+              render={(field) => (
+                <FormInput
+                  label="Expire Date"
+                  control={control}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+            <Controller
+              name="note"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: 'Please add a note .',
+              }}
+              render={(field) => (
+                <FormInput
+                  label="Note"
+                  control={control}
+                  {...field}
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid
+            size={{ xs: 6, sm: 6, md: 6 }}
+            my={5}>
+            <Divider sx={{ borderStyle: 'dashed' }} />
           </Grid>
 
           <Grid size={{ xs: 2, sm: 2, md: 2 }}>

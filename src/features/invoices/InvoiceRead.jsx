78f697ed78ba0015fcaaab102149618d989c986d
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-07-06 21:04:28 +0330

    Add a table for invoice items

--- src/features/invoices/InvoiceRead.jsx
+++ src/features/invoices/InvoiceRead.jsx
@@ -1,24 +1,63 @@
-import { Box, Grid, Paper, Stack, styled, Typography } from '@mui/material';
+import {
+  Chip,
+  Divider,
+  Grid,
+  Paper,
+  Stack,
+  styled,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { useParams } from 'react-router';
-
-const SummaryBox = styled(Box)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  color: (theme.vars ?? theme).palette.text.secondary,
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
-  }),
-}));
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(4),
+  maxWidth: '100%',
   color: (theme.vars ?? theme).palette.text.secondary,
   ...theme.applyStyles('dark', {
     backgroundColor: '#1A2027',
   }),
 }));
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 export default function InvoiceRead() {
   const { readId } = useParams();
@@ -209,7 +248,47 @@ export default function InvoiceRead() {
           </Grid>
         </Grid>
       </Item>
-      <Item sx={{ flex: '100%' }}>Item 3</Item>
+      <Item sx={{ flex: '100%' }}>
+        <Divider
+          textAlign="left"
+          sx={{ mb: 3 }}>
+          <Chip
+            label="Products"
+            size="small"
+          />
+        </Divider>
+
+        <TableContainer component={Paper}>
+          <Table
+            sx={{ minWidth: 700 }}
+            aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Product</StyledTableCell>
+                <StyledTableCell align="right">Price</StyledTableCell>
+                <StyledTableCell align="right">Quantity</StyledTableCell>
+                <StyledTableCell align="right">Total</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell
+                    component="th"
+                    scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {row.calories}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                  <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Item>
     </Stack>
   );
 }

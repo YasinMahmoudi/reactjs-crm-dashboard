f3b6066fb109eebcd993d5b6f78503b697283e01
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-07-31 20:46:09 +0330

    Add payment edit toolbar

---
+++ src/features/payments/PaymentEditToolbar.jsx
@@ -0,0 +1,79 @@
+import Typography from '@mui/material/Typography';
+import MoveBackButton from '../../components/MoveBackButton';
+import { Row } from '../../components/Row';
+
+import { useNavigate } from 'react-router';
+import ContextMenu from '../../components/ContextMenu';
+
+import CloseOutlined from '@mui/icons-material/CloseOutlined';
+import PictureAsPdfOutlined from '@mui/icons-material/PictureAsPdfOutlined';
+
+import { useGetPayment } from './useGetPayment';
+import { CircularProgress } from '@mui/material';
+
+const mainRowStyle = { flexDirection: { xs: 'column', sm: 'row' }, mb: 5 };
+const actionRowStyle = { gap: '10px', flexWrap: 'wrap' };
+
+export default function PaymentEditToolbar() {
+  const navigate = useNavigate();
+
+  const { payment , isLoadingPayment } = useGetPayment();
+
+  if (isLoadingPayment) return <CircularProgress />;
+  
+
+  const {
+    number,
+    invoice: { year, paymentStatus, _id: invoiceId },
+  } = payment;
+
+
+
+  function handleShowInvoice() {
+    navigate(`invoices/read/${invoiceId}`);
+  }
+
+  return (
+    <Row sx={mainRowStyle}>
+      <Row>
+        <MoveBackButton />
+        <Row
+          gap={2}
+          alignItems="center">
+          <Typography
+            variant="caption"
+            fontWeight={600}
+            fontSize="larger">
+            Update Payment # {number}/{year}
+          </Typography>
+
+          <Typography
+            variant="caption"
+            textTransform="capitalize"
+            fontSize="medium"
+            fontWeight={600}>
+            {paymentStatus}
+          </Typography>
+        </Row>
+      </Row>
+
+      <Row sx={actionRowStyle}>
+        <ContextMenu
+          options={[
+            {
+              name: 'Show Invoice',
+              icon: <PictureAsPdfOutlined fontSize="10px" />,
+              onClick: handleShowInvoice,
+            },
+
+            {
+              name: 'Close',
+              icon: <CloseOutlined fontSize="10px" />,
+              onClick: () => navigate('/payment'),
+            },
+          ]}
+        />
+      </Row>
+    </Row>
+  );
+}
--- src/pages/Payments/edit.jsx
+++ src/pages/Payments/edit.jsx
@@ -1,21 +1,26 @@
 import { Grid } from '@mui/material';
 import PaymentUpdateForm from '../../features/payments/PaymentUpdateForm';
 import PaymentClientInfo from '../../features/payments/PaymentClientInfo';
+import PaymentEditToolbar from '../../features/payments/PaymentEditToolbar';
 
 export default function PaymentEdit() {
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 4 }}
-      columns={{ xs: 2, sm: 4, md: 6 }}
-      mt={5}>
-      <Grid size={{ xs: 2, sm: 2, md: 3 }}>
-        <PaymentUpdateForm />
-      </Grid>
+    <>
+      <PaymentEditToolbar />
 
-      <Grid size={{ xs: 2, sm: 2, md: 3 }}>
-        <PaymentClientInfo />
+      <Grid
+        container
+        spacing={{ xs: 2, md: 4 }}
+        columns={{ xs: 2, sm: 4, md: 6 }}
+        mt={5}>
+        <Grid size={{ xs: 2, sm: 2, md: 3 }}>
+          <PaymentUpdateForm />
+        </Grid>
+
+        <Grid size={{ xs: 2, sm: 2, md: 3 }}>
+          <PaymentClientInfo />
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 }

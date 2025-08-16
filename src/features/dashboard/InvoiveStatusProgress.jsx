a7c613197bdde37a9ac17bd63d1a891dc6b3adf6
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-16 19:49:33 +0330

    Complete  invoice progress status

--- src/features/dashboard/InvoiveStatusProgress.jsx
+++ src/features/dashboard/InvoiveStatusProgress.jsx
@@ -1,10 +1,41 @@
-import { Paper } from '@mui/material';
+import { Paper, Stack, Typography } from '@mui/material';
 import LinearProgressWithLabel from '../../components/LinearProgressWithLabel';
 
 export default function InvoiveStatusProgress() {
-  return <Paper sx={{ mt: 10 , p:5 }}>
-    <LinearProgressWithLabel title='Draft' value={60}/>
-  </Paper>;
+  return (
+    <Paper sx={{ mt: 10, p: 5 }}>
+      <Typography
+        variant="h5"
+        mb={4}>
+        Invoices
+      </Typography>
+
+      <Stack spacing={3}>
+        <LinearProgressWithLabel
+          title="Draft"
+          value={60}
+        />
+        <LinearProgressWithLabel
+          title="Pending"
+          value={0}
+        />
+        <LinearProgressWithLabel
+          title="Overdue"
+          value={100}
+        />
+        <LinearProgressWithLabel
+          title="Paid"
+          value={33}
+        />
+        <LinearProgressWithLabel
+          title="Unpaid"
+          value={5}
+        />
+        <LinearProgressWithLabel
+          title="Partially"
+          value={68}
+        />
+      </Stack>
+    </Paper>
+  );
 }
-
-

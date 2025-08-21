679f5ea0a5618b96a34b4996622ecac43830e584
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-21 23:30:38 +0330

    Create invoice summary custom hook

---
+++ src/features/dashboard/useInvoiceSummary.js
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRef } from 'react';
+import { getInvoiceSummaryService } from '../../services/dashboard/invoiceSummary';
+
+function useInvoicesSummary() {
+  const abortControllerRef = useRef();
+
+  const { data: { result } = {}, isLoading: isLoadingInvoicesSummary } =
+    useQuery({
+      queryKey: ['invoicesSummary'],
+      queryFn: () => {
+        if (abortControllerRef.current) {
+          abortControllerRef.current.abort();
+        }
+
+        abortControllerRef.current = new AbortController();
+
+        return getInvoiceSummaryService({
+          signal: abortControllerRef.current.signal,
+        });
+      },
+    });
+
+  return {
+    invoices: result,
+    isLoadingInvoicesSummary,
+  };
+}
+
+export { useInvoicesSummary };

3269534cc690ae68c04bd8c2e5fe117a8727dea5
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-21 17:54:05 +0330

    Create invoice summary service

---
+++ src/services/dashboard/invoiceSummary.js
@@ -0,0 +1,21 @@
+import { API_URL } from '../../utils/constants';
+
+export async function getInvoicesService({ signal }) {
+  try {
+    const res = await fetch(`${API_URL}/invoice/summary?currency=USD`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      signal,
+    });
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(data.message);
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+}

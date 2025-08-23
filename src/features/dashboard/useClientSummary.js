9b363f3f348dcfbe4138803fbaf973df5ebfe693
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-23 19:20:25 +0330

    Update useClientSummary hook

--- src/features/dashboard/useClientSummary.js
+++ src/features/dashboard/useClientSummary.js
@@ -1,30 +1,31 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { useRef } from 'react';
 import { getClientSummaryService } from '../../services/dashboard/clientSummary';
 
 function useClientSummary() {
   const abortControllerRef = useRef();
 
-  const { data: { result } = {}, isLoading: isLoadingClientSummary } =
-    useQuery({
-      queryKey: ['clientSummary'],
-      queryFn: () => {
-        if (abortControllerRef.current) {
-          abortControllerRef.current.abort();
-        }
+  const {
+    data: { result },
+  } = useSuspenseQuery({
+    queryKey: ['clientSummary'],
+    queryFn: () => {
+      if (abortControllerRef.current) {
+        abortControllerRef.current.abort();
+      }
 
-        abortControllerRef.current = new AbortController();
+      abortControllerRef.current = new AbortController();
 
-        return getClientSummaryService({
-          signal: abortControllerRef.current.signal,
-        });
-      },
-    });
+      return getClientSummaryService({
+        signal: abortControllerRef.current.signal,
+      });
+    },
+  });
 
   return {
     clientSummary: result,
-    isLoadingClientSummary,
   };
 }
 
 export { useClientSummary };
+

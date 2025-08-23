c4e9f246e0807d4df049fa32600334f138a8a4d3
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-24 02:33:28 +0330

    Refactore customer create form component

--- src/features/customers/CustomerCreateForm.jsx
+++ src/features/customers/CustomerCreateForm.jsx
@@ -1,17 +1,19 @@
-import { Button, Grid } from '@mui/material';
-import PropTypes from 'prop-types';
+import { Box, Button, Grid } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
 import { useSearchParams } from 'react-router';
+import FormInput from '../../components/FormInput';
+import Select from '../../components/Select';
 import { useCreateCustomer } from './useCreateCustomer';
+import { useGetCustomer } from './useGetCustomer';
 import { useUpdateCustomer } from './useUpdateCustomer';
 
-CustomerCreateForm.propTypes = {
-  children: PropTypes.object,
-  handleSubmit: PropTypes.func,
-};
 
-function CustomerCreateForm({ children, handleSubmit }) {
+function CustomerCreateForm() {
+  const { handleSubmit, control } = useForm();
+
   const [searchParams] = useSearchParams();
 
+  const { customer } = useGetCustomer();
   const { createCustomer, isCreatingCustomer } = useCreateCustomer();
   const { updateCustomer, isUpdatingCustomer } = useUpdateCustomer();
 
@@ -27,7 +29,117 @@ function CustomerCreateForm({ children, handleSubmit }) {
         container
         spacing={{ xs: 2, md: 2 }}
         columns={{ xs: 1, sm: 4 }}>
-        {children}
+        <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+          <Controller
+            name="name"
+            defaultValue={customer?.name}
+            control={control}
+            rules={{
+              required: 'Please add a name .',
+            }}
+            render={(field) => (
+              <FormInput
+                label="Name"
+                control={control}
+                {...field}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+          <Controller
+            name="email"
+            defaultValue={customer?.email}
+            control={control}
+            rules={{
+              required: 'Please provide a valid email.',
+            }}
+            render={(field) => (
+              <FormInput
+                label="Email"
+                control={control}
+                {...field}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+          <Controller
+            name="phone"
+            defaultValue={customer?.phone}
+            control={control}
+            rules={{
+              required: 'Please add a phone number.',
+            }}
+            render={(field) => (
+              <FormInput
+                label="Phone"
+                control={control}
+                {...field}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid size={{ xs: 2, sm: 2, md: 2 }}>
+          <Controller
+            control={control}
+            name="country"
+            defaultValue={customer?.country}
+            rules={{
+              required: 'Please select a country .',
+            }}
+            render={(field) => (
+              <Select
+                isEditing={isEditing}
+                control={control}
+                defaultValue={customer?.country}
+                renderOption={(props, option) => {
+                  // eslint-disable-next-line react/prop-types
+                  const { key, ...optionProps } = props;
+
+                  return (
+                    <Box
+                      key={key}
+                      component="li"
+                      sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+                      {...optionProps}>
+                      <img
+                        loading="lazy"
+                        width="20"
+                        srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+                        src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+                        alt={`Country ${option.label}`}
+                      />
+                      {option.label} ({option.code}) +{option.phone}
+                    </Box>
+                  );
+                }}
+                {...field}
+              />
+            )}
+          />
+        </Grid>
+
+        <Grid size={{ xs: 2, sm: 4 }}>
+          <Controller
+            name="address"
+            defaultValue={customer?.address}
+            control={control}
+            rules={{
+              required: 'Please add an address.',
+            }}
+            render={(field) => (
+              <FormInput
+                label="Address"
+                control={control}
+                {...field}
+              />
+            )}
+          />
+        </Grid>
 
         <Grid size={{ xs: 2, sm: 2, md: 2 }}>
           <Button
--- src/features/customers/CustomerCreateModal.jsx
+++ src/features/customers/CustomerCreateModal.jsx
@@ -1,146 +1,17 @@
-import CircularProgress from '@mui/material/CircularProgress';
-import Grid from '@mui/material/Grid';
-
-import Box from '@mui/material/Box';
-
-import { Controller, useForm } from 'react-hook-form';
 import { useSearchParams } from 'react-router';
-import FormInput from '../../components/FormInput';
 import EnhancedModal from '../../components/Modal';
-import Select from '../../components/Select';
 import CustomerCreateForm from './CustomerCreateForm';
-import { useGetCustomer } from './useGetCustomer';
+import { Suspense } from 'react';
 
 export default function CustomerCreateModal() {
-  const { handleSubmit, control } = useForm();
-
   const [searchParams] = useSearchParams();
-
-  const { customer, isLoadingCustomer } = useGetCustomer();
-
   const isEditing = searchParams.get('edit') === 'true';
-
-  if (isEditing && isLoadingCustomer) return <CircularProgress />;
 
   return (
     <EnhancedModal title={isEditing ? `Edit customer` : 'Add new customer'}>
-      {isEditing && isLoadingCustomer ? (
-        <CircularProgress />
-      ) : (
-        <CustomerCreateForm handleSubmit={handleSubmit}>
-          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
-            <Controller
-              name="name"
-              defaultValue={customer?.name}
-              control={control}
-              rules={{
-                required: 'Please add a name .',
-              }}
-              render={(field) => (
-                <FormInput
-                  label="Name"
-                  control={control}
-                  {...field}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
-            <Controller
-              name="email"
-              defaultValue={customer?.email}
-              control={control}
-              rules={{
-                required: 'Please provide a valid email.',
-              }}
-              render={(field) => (
-                <FormInput
-                  label="Email"
-                  control={control}
-                  {...field}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
-            <Controller
-              name="phone"
-              defaultValue={customer?.phone}
-              control={control}
-              rules={{
-                required: 'Please add a phone number.',
-              }}
-              render={(field) => (
-                <FormInput
-                  label="Phone"
-                  control={control}
-                  {...field}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid size={{ xs: 2, sm: 2, md: 2 }}>
-            <Controller
-              control={control}
-              name="country"
-              defaultValue={customer?.country}
-              rules={{
-                required: 'Please select a country .',
-              }}
-              render={(field) => (
-                <Select
-                  isEditing={isEditing}
-                  control={control}
-                  defaultValue={customer?.country}
-                  renderOption={(props, option) => {
-                    // eslint-disable-next-line react/prop-types
-                    const { key, ...optionProps } = props;
-
-                    return (
-                      <Box
-                        key={key}
-                        component="li"
-                        sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
-                        {...optionProps}>
-                        <img
-                          loading="lazy"
-                          width="20"
-                          srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-                          src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-                          alt={`Country ${option.label}`}
-                        />
-                        {option.label} ({option.code}) +{option.phone}
-                      </Box>
-                    );
-                  }}
-                  {...field}
-                />
-              )}
-            />
-          </Grid>
-
-          <Grid size={{ xs: 2, sm: 4 }}>
-            <Controller
-              name="address"
-              defaultValue={customer?.address}
-              control={control}
-              rules={{
-                required: 'Please add an address.',
-              }}
-              render={(field) => (
-                <FormInput
-                  label="Address"
-                  control={control}
-                  {...field}
-                />
-              )}
-            />
-          </Grid>
-        </CustomerCreateForm>
-      )}
+      <Suspense fallback={<p>Loading...</p>}>
+        <CustomerCreateForm />
+      </Suspense>
     </EnhancedModal>
   );
 }

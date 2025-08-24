8ab65694584ad34342435e5e5c82b8651c51b0f5
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-24 22:51:31 +0330

    Update useGetInvoice hook

--- src/features/invoices/useGetInvoice.js
+++ src/features/invoices/useGetInvoice.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { useIsEditing } from '../../hooks/useIsEditing';
 import { getInvoiceService } from '../../services/invoices';
@@ -8,13 +8,14 @@ function useGetInvoice() {
 
   const selectedId = isEditing ? editId : readId;
 
-  const { data: invoice = {}, isLoading: isLoadingInvoice } = useQuery({
+  const { data: invoice = {} } = useSuspenseQuery({
     queryKey: ['invoice', selectedId],
     queryFn: () => getInvoiceService(selectedId),
     enabled: selectedId !== null && selectedId !== undefined,
   });
 
-  return { invoice, isLoadingInvoice };
+  return { invoice };
 }
 
 export { useGetInvoice };
+

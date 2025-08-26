3c04dd0826b3b14cd934294a12668b274b426f33
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-26 22:40:57 +0330

    Add payment read toolbar skeleton

---
+++ src/components/Skeletons/payments/PaymentToolbarActionsSkeleton.jsx
@@ -0,0 +1,10 @@
+import { IconButton } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+export default function PaymentToolbarActionsSkeleton() {
+  return (
+    <IconButton>
+      <MoreVertIcon />
+    </IconButton>
+  );
+}
--- src/features/payments/PaymentReadToolbar.jsx
+++ src/features/payments/PaymentReadToolbar.jsx
@@ -8,24 +8,67 @@ import ContextMenu from '../../components/ContextMenu';
 import CloseOutlined from '@mui/icons-material/CloseOutlined';
 import EditIcon from '@mui/icons-material/EditOutlined';
 import PictureAsPdfOutlined from '@mui/icons-material/PictureAsPdfOutlined';
+import { Suspense } from 'react';
+import PaymentToolbarActionsSkeleton from '../../components/Skeletons/payments/PaymentToolbarActionsSkeleton';
+import TextSkeleton from '../../components/Skeletons/TextSkeleton';
 import { DOWNLOAD_BASE_URL } from '../../utils/constants';
-
-import PropTypes from 'prop-types';
-
-PaymentReadToolbar.propTypes = {
-  payment: PropTypes.object,
-};
+import { useGetPayment } from './useGetPayment';
 
 const mainRowStyle = { flexDirection: { xs: 'column', sm: 'row' }, mb: 5 };
 const actionRowStyle = { gap: '10px', flexWrap: 'wrap' };
 
-export default function PaymentReadToolbar({ payment }) {
-  const navigate = useNavigate();
+export default function PaymentReadToolbar() {
+  return (
+    <Row sx={mainRowStyle}>
+      <Row>
+        <MoveBackButton />
+
+        <Suspense fallback={<TextSkeleton />}>
+          <PaymentToolbarStatus />
+        </Suspense>
+      </Row>
+
+      <Suspense fallback={<PaymentToolbarActionsSkeleton />}>
+        <PaymentToolbarActions />
+      </Suspense>
+    </Row>
+  );
+}
+
+function PaymentToolbarStatus() {
+  const { payment } = useGetPayment();
 
   const {
     number,
     invoice: { year, paymentStatus },
   } = payment;
+
+  return (
+    <Row
+      gap={2}
+      alignItems="center">
+      <Typography
+        variant="caption"
+        fontWeight={600}
+        fontSize="larger">
+        payment # {number}/{year}
+      </Typography>
+
+      <Typography
+        variant="caption"
+        textTransform="capitalize"
+        fontSize="medium"
+        fontWeight={600}>
+        {paymentStatus}
+      </Typography>
+    </Row>
+  );
+}
+
+function PaymentToolbarActions() {
+  const navigate = useNavigate();
+
+  const { payment } = useGetPayment();
 
   function handleEdit() {
     navigate(`/payment/edit/${payment._id}`);
@@ -39,52 +82,28 @@ export default function PaymentReadToolbar({ payment }) {
   }
 
   return (
-    <Row sx={mainRowStyle}>
-      <Row>
-        <MoveBackButton />
-        <Row
-          gap={2}
-          alignItems="center">
-          <Typography
-            variant="caption"
-            fontWeight={600}
-            fontSize="larger">
-            payment # {number}/{year}
-          </Typography>
+    <Row sx={actionRowStyle}>
+      <ContextMenu
+        options={[
+          {
+            name: 'Edit',
+            icon: <EditIcon fontSize="10px" />,
+            onClick: handleEdit,
+          },
 
-          <Typography
-            variant="caption"
-            textTransform="capitalize"
-            fontSize="medium"
-            fontWeight={600}>
-            {paymentStatus}
-          </Typography>
-        </Row>
-      </Row>
+          {
+            name: 'Download Pdf',
+            icon: <PictureAsPdfOutlined fontSize="10px" />,
+            onClick: handleDownloadPdf,
+          },
 
-      <Row sx={actionRowStyle}>
-        <ContextMenu
-          options={[
-            {
-              name: 'Edit',
-              icon: <EditIcon fontSize="10px" />,
-              onClick: handleEdit,
-            },
-
-            {
-              name: 'Download Pdf',
-              icon: <PictureAsPdfOutlined fontSize="10px" />,
-              onClick: handleDownloadPdf,
-            },
-
-            {
-              name: 'Close',
-              icon: <CloseOutlined fontSize="10px" />,
-              onClick: () => navigate('/payment'),
-            },
-          ]}
-        />
-      </Row>
+          {
+            name: 'Close',
+            icon: <CloseOutlined fontSize="10px" />,
+            onClick: () => navigate('/payment'),
+          },
+        ]}
+      />
     </Row>
   );
 }

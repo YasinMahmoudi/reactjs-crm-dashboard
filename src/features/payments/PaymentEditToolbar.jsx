19792beb37ace7ea7ab26166ad8d9ce18dfeff81
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-08-28 22:22:30 +0330

    Add skeleton loading in payment edit toolbar

--- src/features/payments/PaymentEditToolbar.jsx
+++ src/features/payments/PaymentEditToolbar.jsx
@@ -9,71 +9,124 @@ import CloseOutlined from '@mui/icons-material/CloseOutlined';
 import PictureAsPdfOutlined from '@mui/icons-material/PictureAsPdfOutlined';
 
 import { useGetPayment } from './useGetPayment';
-import { CircularProgress } from '@mui/material';
+
+import TextSkeleton from '../../components/Skeletons/TextSkeleton';
+import { Suspense } from 'react';
+import { IconButton } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const mainRowStyle = { flexDirection: { xs: 'column', sm: 'row' }, mb: 5 };
 const actionRowStyle = { gap: '10px', flexWrap: 'wrap' };
 
 export default function PaymentEditToolbar() {
-  const navigate = useNavigate();
+  return (
+    <Row sx={mainRowStyle}>
+      <Row>
+        <MoveBackButton />
 
-  const { payment , isLoadingPayment } = useGetPayment();
+        <Suspense fallback={<PaymentEditToolbarStatusSkeleton />}>
+          <Row
+            gap={2}
+            alignItems="center">
+            <PaymentEditToolbarStatus />
+          </Row>
+        </Suspense>
+      </Row>
 
-  if (isLoadingPayment) return <CircularProgress />;
-  
+      <Row sx={actionRowStyle}>
+        <Suspense
+          fallback={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }>
+          <PaymentActions />
+        </Suspense>
+      </Row>
+    </Row>
+  );
+}
+
+function PaymentEditToolbarStatusSkeleton() {
+  return (
+    <>
+      <Typography
+        variant="caption"
+        fontWeight={600}
+        fontSize="larger"
+        display='flex'
+        gap={2}
+        >
+        Update Payment # <TextSkeleton />
+      </Typography>
+      <Typography
+        variant="caption"
+        textTransform="capitalize"
+        fontSize="medium"
+        fontWeight={600}
+        ml={2}
+        >
+        <TextSkeleton />
+      </Typography>
+    </>
+  );
+}
+
+function PaymentEditToolbarStatus() {
+  const { payment } = useGetPayment();
 
   const {
     number,
-    invoice: { year, paymentStatus, _id: invoiceId },
+    invoice: { year, paymentStatus },
   } = payment;
 
+  return (
+    <>
+      <Typography
+        variant="caption"
+        fontWeight={600}
+        fontSize="larger">
+        Update Payment # {number}/{year}
+      </Typography>
+      <Typography
+        variant="caption"
+        textTransform="capitalize"
+        fontSize="medium"
+        fontWeight={600}>
+        {paymentStatus}
+      </Typography>
+    </>
+  );
+}
 
+function PaymentActions() {
+  const navigate = useNavigate();
+
+  const { payment } = useGetPayment();
+
+  const {
+    invoice: { _id: invoiceId },
+  } = payment;
 
   function handleShowInvoice() {
     navigate(`/invoices/read/${invoiceId}`);
   }
 
   return (
-    <Row sx={mainRowStyle}>
-      <Row>
-        <MoveBackButton />
-        <Row
-          gap={2}
-          alignItems="center">
-          <Typography
-            variant="caption"
-            fontWeight={600}
-            fontSize="larger">
-            Update Payment # {number}/{year}
-          </Typography>
+    <ContextMenu
+      options={[
+        {
+          name: 'Show Invoice',
+          icon: <PictureAsPdfOutlined fontSize="10px" />,
+          onClick: handleShowInvoice,
+        },
 
-          <Typography
-            variant="caption"
-            textTransform="capitalize"
-            fontSize="medium"
-            fontWeight={600}>
-            {paymentStatus}
-          </Typography>
-        </Row>
-      </Row>
-
-      <Row sx={actionRowStyle}>
-        <ContextMenu
-          options={[
-            {
-              name: 'Show Invoice',
-              icon: <PictureAsPdfOutlined fontSize="10px" />,
-              onClick: handleShowInvoice,
-            },
-
-            {
-              name: 'Close',
-              icon: <CloseOutlined fontSize="10px" />,
-              onClick: () => navigate('/payment'),
-            },
-          ]}
-        />
-      </Row>
-    </Row>
+        {
+          name: 'Close',
+          icon: <CloseOutlined fontSize="10px" />,
+          onClick: () => navigate('/payment'),
+        },
+      ]}
+    />
   );
 }

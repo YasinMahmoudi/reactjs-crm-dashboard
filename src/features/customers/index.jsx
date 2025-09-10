fcb4c1d40f3c090c392284342317c56eeb776e94
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-09-10 13:02:18 +0330

    Refactor customer toolbar

--- src/features/customers/index.jsx
+++ src/features/customers/index.jsx
@@ -1,14 +1,17 @@
 import Box from '@mui/material/Box';
 
+import Toolbar from '../core/Toolbar';
+import CustomerSearch from './CustomerSearch';
 import CustomersTable from './CustomersTable';
-import CustomerToolbar from './CustomerToolbar';
 
 const tableBoxStyle = { mt: 3 };
 
 export default function Customers() {
   return (
     <>
-      <CustomerToolbar />
+      <Toolbar buttonLabel="Add New Client">
+        <CustomerSearch />
+      </Toolbar>
 
       <Box sx={tableBoxStyle}>
         <CustomersTable />

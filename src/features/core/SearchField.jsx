1f0eded234a9cc03f43472819ef86546b6e88918
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-09-10 13:28:50 +0330

    Create a reusable search field component

---
+++ src/features/core/SearchField.jsx
@@ -0,0 +1,50 @@
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router';
+import PropTypes from 'prop-types';
+
+export default function SearchField({ id }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [query, setQuery] = useState(function () {
+    return searchParams.get('query') || '';
+  });
+
+  function handleQueyParam(e) {
+    setQuery(e.target.value);
+
+    if (searchParams.has('query') && e.target.value.length === 0) {
+      searchParams.delete('query');
+      setSearchParams(searchParams);
+
+      return;
+    }
+
+    searchParams.set('page', '1');
+    searchParams.set('query', e.target.value);
+
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <TextField
+      id={id}
+      label="Search"
+      type="search"
+      variant="outlined"
+      size="small"
+      sx={{
+        order: { xs: '3', sm: '0' },
+        flexGrow: { xs: '1', sm: 'initial' },
+        marginTop: { xs: '8px', sm: '0' },
+        width: { xs: '100%', sm: 'auto' },
+      }}
+      value={query}
+      onChange={handleQueyParam}
+    />
+  );
+}
+
+SearchField.propTypes = {
+  id: PropTypes.string,
+};

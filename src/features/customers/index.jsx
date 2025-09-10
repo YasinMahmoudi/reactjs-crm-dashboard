4fe10e42152467c562dfb012f84800075b802d5b
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-09-10 13:29:40 +0330

    Refactor customer search

--- src/features/customers/index.jsx
+++ src/features/customers/index.jsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 
+import SearchField from '../core/SearchField';
 import Toolbar from '../core/Toolbar';
-import CustomerSearch from './CustomerSearch';
 import CustomersTable from './CustomersTable';
 
 const tableBoxStyle = { mt: 3 };
@@ -9,8 +9,10 @@ const tableBoxStyle = { mt: 3 };
 export default function Customers() {
   return (
     <>
-      <Toolbar buttonLabel="Add New Client" to="/customers/create">
-        <CustomerSearch />
+      <Toolbar
+        buttonLabel="Add New Client"
+        to="/customers/create">
+        <SearchField id="customerSearchInput" />
       </Toolbar>
 
       <Box sx={tableBoxStyle}>

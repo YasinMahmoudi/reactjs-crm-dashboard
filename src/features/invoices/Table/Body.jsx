5724bc61fa0380a0dee8c4b3aa8577b20b650ee7
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-09 18:39:57 +0330

    Update invoice table body

--- src/features/invoices/Table/Body.jsx
+++ src/features/invoices/Table/Body.jsx
@@ -1,5 +1,5 @@
-import DataTable from '../../components/Table';
-import InvoiceTableRow from './InvoiceTableRow';
+import DataTable from '../../../components/Table';
+import InvoiceTableRow from './Row';
 
 export default function InvoiceTableBody() {
   return <DataTable.Body render={(row) => <InvoiceTableRow key={row._id} row={row} />} />;

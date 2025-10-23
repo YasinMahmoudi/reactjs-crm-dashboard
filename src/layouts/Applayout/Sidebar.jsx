27f08e9ad059bdc6c98956517981c1762c26dab1
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-24 00:05:01 +0330

    Update sidebar dark styles

--- src/layouts/Applayout/Sidebar.jsx
+++ src/layouts/Applayout/Sidebar.jsx
@@ -1,18 +1,24 @@
 import Paper from '@mui/material/Paper';
 
-import styled from 'styled-components';
+import { styled } from '@mui/material/styles';
 
-const StyledSidebar = styled.aside`
-  grid-column: 1 / 2;
-  grid-row: 1/ -1;
-  padding: max(2rem, 2vw);
-  display: none;
+const StyledSidebar = styled('aside')(({ theme }) => [
+  {
+    gridColumn: '1 / 2',
+    gridRow: ' 1 / -1',
+    padding: 'max(2rem ,  2vw)',
+    display: 'none',
 
-  @media screen and (min-width: 900px) {
-    display: block;
-    border-right: 1px solid #e3e3e3;
-  }
-`;
+    '@media screen and (min-width: 900px)': {
+      display: 'block',
+      borderRight: '1px solid #e3e3e3',
+    },
+  },
+
+  theme.applyStyles('dark', {
+      borderRight: '1px solid #363434 !important',
+  }),
+]);
 
 const StyledPaper = styled(Paper)`
   background-color: transparent !important;

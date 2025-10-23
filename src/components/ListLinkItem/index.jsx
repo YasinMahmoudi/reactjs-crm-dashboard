f07bbe7de04527f58acc0f2a3a1720c6e0c39bc6
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-23 23:25:10 +0330

    Add dark mode style for list item

--- src/components/ListLinkItem/index.jsx
+++ src/components/ListLinkItem/index.jsx
@@ -2,23 +2,26 @@ import { NavLink } from 'react-router';
 
 import ListItemButton from '@mui/material/ListItemButton';
 import { forwardRef } from 'react';
-import styled from 'styled-components';
+import { styled } from '@mui/material';
 
+const StyledListItemLink = styled(ListItemButton)(({ theme }) => [
+  {
+    color: '#4e4b4b',
+    borderRadius: 3,
+    marginBottom:2,
+    '&:hover , &:active , &.active:link , &.active:visited': {
+      color: '#f1f1f1',
+      backgroundColor: theme.palette.primary.main,
+      '& svg': {
+        fill: '#f1f1f1',
+      },
+    },
+  },
 
-
-const StyledListItemLink = styled(ListItemButton)`
-  &:hover,
-  &:active,
-  &.active:link,
-  &.active:visited {
-    color: #f1f1f1;
-    background-color: #1241cc !important;
-
-    & svg {
-      fill: #f1f1f1;
-    }
-  }
-`;
+  theme.applyStyles('dark', {
+    color: '#bab9b9',
+  }),
+]);
 
 // eslint-disable-next-line react/display-name
 const CustomNavLink = forwardRef((props, ref) => (

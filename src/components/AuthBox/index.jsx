e663b88b7c1293c14fc71aeb1e92c4d48d12d8f5
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-24 00:51:21 +0330

    Add dark mode styles for auth forms

--- src/components/AuthBox/index.jsx
+++ src/components/AuthBox/index.jsx
@@ -1,33 +1,41 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
-
-
-const style = {
-  width: ' min(90% , 30rem)',
-  minHeight: '450px',
-  backgroundColor: '',
-  color: '#fff',
-  marginInline: 'auto',
-  borderRadius: '20px',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  gap: '20px',
-  boxShadow: '2px 2px 4px rgba(0,0,0,.2)',
-  padding: 'max(1.5rem , 2vw)',
-};
+import ModeToggle from '../ModeToggle';
 
 const titleStyle = {
-  color: '#141414',
+  // color: '#141414',
   fontWeight: '700',
   marginBottom: '30px',
 };
 
 export default function AuthBox({ children, title }) {
   return (
-    <Box sx={style}>
+    <Box
+      sx={[
+        (theme) => ({
+          width: ' min(90% , 30rem)',
+          minHeight: '450px',
+          backgroundColor: '',
+          color: '#141414',
+          marginInline: 'auto',
+          borderRadius: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '20px',
+          border: '1px solid #e3e3e3',
+          boxShadow: theme.shadows[1],
+          padding: 'max(1.5rem , 2vw)',
+        }),
+        (theme) =>
+          theme.applyStyles('dark', {
+            boxShadow: theme.shadows[24],
+            border: '1px solid #323232',
+            color: '#f1f1f1',
+          }),
+      ]}>
+      <ModeToggle />
       <Typography
         variant="h4"
         sx={titleStyle}>

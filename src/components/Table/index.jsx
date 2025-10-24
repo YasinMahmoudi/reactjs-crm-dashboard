259c69d8e28c0ad90a8d423914e50688472604e8
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-24 17:28:04 +0330

    Update table dark mode styles

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -43,7 +43,7 @@ export default function Table({
         onDeleteMultipleRecords,
         hasChechBox
       }}>
-      <Paper sx={{ width: '100%', mb: 2, overflow: 'hidden' }}>
+      <Paper sx={{ width: '100%', mb: 2, overflow: 'hidden' , boxShadow:theme => theme.applyStyles('dark', { boxShadow: '0px 0px 1px rgba(255,255,255,0.5)' }) }}>
         {state ? (
           <TableLoader />
         ) : (

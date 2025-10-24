85b8031183eb9a4baf50b032bccf6bc55188fa66
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-24 19:23:55 +0330

    Refactor tax toolbar

--- src/features/tax/index.jsx
+++ src/features/tax/index.jsx
@@ -1,13 +1,19 @@
 import { Box } from '@mui/material';
-import TaxTable from './TaxTable';
-import TaxToolbar from './TaxToolbar';
+import TaxTable from './Table';
+import Toolbar from '../core/Toolbar';
+import SearchField from '../core/SearchField';
+
 
 const tableBoxStyle = { mt: 3 };
 
 export default function Tax() {
   return (
     <>
-      <TaxToolbar />
+      <Toolbar
+        buttonLabel="Add New Payment Mode"
+        to="/payment/mode/create">
+        <SearchField id="taxSearchInput" />
+      </Toolbar>
 
       <Box sx={tableBoxStyle}>
         <TaxTable />

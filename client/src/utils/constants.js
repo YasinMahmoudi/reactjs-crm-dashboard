75de36883c1ab053bd65f584c5fbdd6d716dbfeb
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-10-24 23:09:45 +0330

    Add production urls

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,9 @@
-export const API_URL = 'http://localhost:8888/api';
-export const UPLOAD_URL = 'http://localhost:8888';
-export const DOWNLOAD_BASE_URL = 'http://localhost:8888/download';
+// export const API_URL = 'http://localhost:8888/api';
+// export const UPLOAD_URL = 'http://localhost:8888';
+// export const DOWNLOAD_BASE_URL = 'http://localhost:8888/download';
+
+export const API_URL = 'https://server-crm-dashboard.onrender.com/api';
+export const UPLOAD_URL = 'https://server-crm-dashboard.onrender.com';
+export const DOWNLOAD_BASE_URL = 'https://server-crm-dashboard.onrender.com/download';
 
 

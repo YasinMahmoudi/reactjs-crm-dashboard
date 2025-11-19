a79989e83bc7fe4b3361ffa0f167c9aeb3bb8678
YasinMahmoudi <yassin.mh.n.998@gmail.com>
2025-11-20 01:45:32 +0330

    Update logo component width and height

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -20,9 +20,9 @@ export default function Logo() {
       <Img
         src="/logo.png"
         alt="Logo"
-        fetchpriority="high"
-        width="140"
-        height="32"
+        fetchPriority="high"
+        width={161}
+        height={37}
       />
     </Link>
   );
